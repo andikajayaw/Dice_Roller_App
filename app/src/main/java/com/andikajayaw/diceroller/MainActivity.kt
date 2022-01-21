3a8d6ea86b7da3e749406ec695e2b058dc692e4f
andikajayaw <andikajayawiguna@gmail.com>
2022-01-21 23:58:52 +0800

    add function to button roll for rolling the dice

--- app/src/main/java/com/andikajayaw/diceroller/MainActivity.kt
+++ app/src/main/java/com/andikajayaw/diceroller/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.andikajayaw.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val firstDice = Dice(6)
+        val firstDiceRoll = firstDice.roll()
+
+        val secondDice = Dice(6)
+        val secondDiceRoll = secondDice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = firstDiceRoll.toString()
+
+        val resultTextView2: TextView = findViewById(R.id.textView2)
+        resultTextView2.text = secondDiceRoll.toString()
+
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
